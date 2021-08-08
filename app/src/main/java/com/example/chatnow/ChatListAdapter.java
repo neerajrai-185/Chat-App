fd24c58eebb01f2c9c95c0b4ddadb4de4ca124f1
neerajrai-185 <raineeraj185@gmail.com>
2021-08-08 08:58:08 +0530

    New things added

---
+++ app/src/main/java/com/example/chatnow/ChatListAdapter.java
@@ -0,0 +1,82 @@
+package com.example.chatnow;
+
+import android.content.Context;
+import android.net.Uri;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.example.chatnow.Common.Constants;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ChatListAdapter extends RecyclerView.Adapter<ChatListAdapter.ChatListViewHolder> {
+
+    private Context context;
+    private List<ChatListModel> chatListModelList;
+
+    public ChatListAdapter(Context context, List<ChatListModel> chatListModelList) {
+        this.context = context;
+        this.chatListModelList = chatListModelList;
+    }
+
+    @NonNull
+    @Override
+    public ChatListAdapter.ChatListViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.chat_list_layout, parent, false);
+        return new ChatListViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ChatListAdapter.ChatListViewHolder holder, int position) {
+        final ChatListModel chatListModel = chatListModelList.get(position);
+
+        holder.tvFullName.setText(chatListModel.getUserName());
+
+        StorageReference fileRef = FirebaseStorage.getInstance().getReference().child(Constants.IMAGE_FOLDER + "/" + chatListModel.getPhotoName());
+        fileRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                Glide.with(context)
+                        .load(uri)
+                        .placeholder(R.drawable.iv_profile)
+                        .error(R.drawable.iv_profile)
+                        .into(holder.ivProfile);
+
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return chatListModelList.size();
+    }
+
+    public class ChatListViewHolder extends RecyclerView.ViewHolder {
+        private LinearLayout llChatList;
+        private TextView tvFullName, tvLastMessage, tvLastMessageTime, tvUnreadCount;
+        private ImageView ivProfile;
+
+        public ChatListViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            llChatList = itemView.findViewById(R.id.llChatList);
+            tvFullName = itemView.findViewById(R.id.tvFullName);
+            tvLastMessage = itemView.findViewById(R.id.tvLastMessage);
+            tvLastMessageTime = itemView.findViewById(R.id.tvLastMessageTime);
+            tvUnreadCount= itemView.findViewById(R.id.tvUnreadCount);
+            ivProfile = itemView.findViewById(R.id.ivProfile);
+        }
+    }
+}
